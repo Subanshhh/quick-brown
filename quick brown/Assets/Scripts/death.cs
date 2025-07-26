7c68572b9ea0402871063306d5d075b8726383ad
Subanshhh <subanshghorasaini@gmail.com>
2025-07-26 15:25:30 +0545

    CAN DIE NOW

---
+++ quick brown/Assets/Scripts/death.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOnTouch : MonoBehaviour
+{
+   
+    public string playerTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(playerTag))
+        {
+            RestartScene();
+        }
+    }
+
+    private void RestartScene()
+    {
+       
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+}
